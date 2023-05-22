a7936d7f75e7b5602d504a74491a10d588ce8a76
Chandima Ranaweera <chandima@bistecglobal.com>
2023-05-22 19:46:19 +0530

    add cors

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 from flask import Flask, request
 from chatbot import chat
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resource={
+    r"/*":{
+        "origins":"*"
+    }
+})
 
 @app.route("/")
 def hello_world():
     text = request.args.get('text')
+    if text is None:
+        return "Please send <strong>text</strong> parameter"
     output = chat(text)
     return f'<h1>{output}!</h1>'
 
 @app.route("/message")
 def message():
     text = request.args.get('text')
+    if text is None:
+        return {
+        "response": "Please send {text} parameter"
+    }
     output = chat(text)
     return {
         "response": output
