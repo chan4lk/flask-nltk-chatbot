062fd04d8983766fbaf96076006938da117ea50d
Chandima Ranaweera <chandima@bistecglobal.com>
2023-05-22 18:57:31 +0530

    chat with flask api

--- Readme.md
+++ Readme.md
@@ -7,3 +7,18 @@ py -m pip install -r requirements.txt
 ```shell
 .\Scripts\activate.bat
 ```
+
+# Train
+```shell
+python trainModel.py
+```
+
+# Run Flask API
+```shell
+python -m flask --app app run
+```
+
+# Call api
+```http
+http://127.0.0.1:5000/?text=Hi
+```
--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, request
+from chatbot import chat
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    text = request.args.get('text')
+    output = chat(text)
+    return f'<h1>{output}!</h1>'
