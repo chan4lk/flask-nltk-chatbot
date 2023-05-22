0c0c238768fbcdf39a0819a98b4d65f8953a6ac0
Chandima Ranaweera <chandima@bistecglobal.com>
2023-05-22 19:07:00 +0530

    endpoint to return json

--- app.py
+++ app.py
@@ -8,3 +8,11 @@ def hello_world():
     text = request.args.get('text')
     output = chat(text)
     return f'<h1>{output}!</h1>'
+
+@app.route("/message")
+def message():
+    text = request.args.get('text')
+    output = chat(text)
+    return {
+        "response": output
+    }
